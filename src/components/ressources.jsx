eb752a4b705e6499fbcdc1c24959722caf4624d3
Robin Lebhar <robin_lebhar@hotmail.fr>
2018-06-28 11:50:40 +0200

    Create ressources.jsx

---
+++ src/components/ressources.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Ressources extends Component {
+  render() {
+    return (
+      <div>
+        Ca c'est de la ressource
+      </div>
+    );
+  }
+}
+
+export default Ressources;
