5c2cd7a57a10cc287161e5d579b9159ab54d10ed
Robin Lebhar <robin_lebhar@hotmail.fr>
2018-06-28 11:49:53 +0200

    Update app.jsx

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,22 +1,21 @@
-import Header from '../containers/header';
-import React, { Component } from 'react'
+import Header from "../containers/header";
+import React, { Component } from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "../components/home";
-import TodoApp from "./todo-app"
+import Ressources from "./ressources";
 require("../style.css");
 export default class App extends Component {
   render() {
     return (
       <div>
-        < Header />
+        <Header />
         <div className="container body_content">
           <Switch>
             <Route exact path="/" component={Home} />
-            <Route path="/ressources" component={TodoApp} />
+            <Route path="/ressources" component={Ressources} />
           </Switch>
         </div>
       </div>
     );
   }
 }
-
