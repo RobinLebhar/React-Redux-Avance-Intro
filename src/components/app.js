99fe74512a1e133ab391be1fc5525d0513dc2734
Robin Lebhar <robin_lebhar@hotmail.fr>
2018-09-22 15:03:15 -0400

    Update app.js

--- src/components/app.js
+++ src/components/app.js
@@ -3,7 +3,7 @@ require("../style.css");
 export default class App extends Component {
   render() {
     return (
-      "Ceci est une app créée avec react-create-app"
+      "Ceci est une app créée avec create-react-app"
     )
   }
 }
